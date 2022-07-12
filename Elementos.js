614f1be2ad976861b713290a38146e665a903a6a
danisteb90 <esteban.alban9003@gmail.com>
2022-07-11 19:49:53 -0500

    first commit

---
+++ Elementos.js
@@ -0,0 +1,13 @@
+const nombre = "Esteban Alban";
+const edad = 32;
+let desarrollador = true;
+const fecha = new Date(1990,2,20);
+let libro = {
+    titulo: "El Mentalista",
+    autor: "Camila Lackberg",
+    paginas: 720,
+    url: "https://www.planetadelibros.com/libro-el-mentalista/347240",
+}
+
+console.log ("Hola, mi nombre es: " + nombre + " tengo " + edad + " anios " + "me considero un desarrollador (" 
++ desarrollador + "), nací el " + fecha + " y mi libro favorito es: " + libro.titulo);    
